c36189f7aef97b312aad3e89692e348c55705344
Şeyma <symagn2@gmail.com>
2023-08-11 10:49:52 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+print("kodlama")
+baslik = "Taşıt Kredisi"
+print(baslik)
+#stringg => metinsel ifade
+baslik = "İhtiyaç Kredisi"
+print(baslik)
+#integer=tam sayı
+vade = 36
+vade2 = "36"
+
+aylikOdeme = 200.50
+
+yukselisteMi = True
+
+# yeniVade = vade / 2
+# print(yeniVade)
+
+sayi1 = 10
+sayi2 = 15
+
+if sayi1 < sayi2:
+    print("sayi1 sayi2 den büyüktür")
+print("burası if bloğunun dışı")     
