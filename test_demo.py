5a826ce1c94cb3c57c7de88b29bc2d8628485652
Şeyma <symagn2@gmail.com>
2023-08-17 15:23:28 +0300

    pytest giriş

---
+++ test_demo.py
@@ -0,0 +1,42 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.action_chains import ActionChains
+
+class Test_DemoClass:
+    #her testten önce çağırılır.
+    def setup_method(self): #genel kruluna ihtiyaç duyulabilmesine yardımcı olur.
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.get("https://www.saucedemo.com/")
+    #her testten sonra çağırılır
+    def teardown_metod(self): #çıkış gibi biirme işlemlerinde yardımcı olr.
+        self.driver.quit() #ilgili yapıyı kapatmama yardımcı olur.
+
+    def readData(self):
+        print("x")
+        #setup -> test_demoFunc -> teardown
+    def test_demoFunc(self):
+        print("demo test")
+        text = "Hello"
+        assert text == "Hello"
+    def test_demo2(self):
+        assert True
+    
+    def test_invalid_login(self):
+        WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID, "user-name")))
+        usernameInput = self.driver.find_element(By.ID, "user-name")
+        WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID, "password")))
+        passwordInput = self.driver.find_element(By.ID, "password")
+        #en fazla 5 saniye olacak şekilde user-name id'li elementin görünmesini bekle
+        usernameInput.send_keys("1")
+        passwordInput.send_keys("1")
+        loginBtn = self.driver.find_element(By.ID,"login-button")
+        loginBtn.click()
+        errorMessage = self.driver.find_element(By.XPATH, "//*[@id='login_button_container']/div/form/div[3]/h3")
+        assert errorMessage.text == "Epic sadface: Username and password do not match any user in this service"
+       
+
+ 
